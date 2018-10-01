540f8dc455b2408993ce12493c61760adde4ca8a
David Kitchen <david.kitchen@commercelabs.io>
2018-10-01 20:13:56 +0100

    Setup for Scout Badges App

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'UK Scout Badges App',
+        short_name: 'UK Scout Badges',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        background_color: '#7413dc',
+        theme_color: '#7413dc',
+        display: 'standalone',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
