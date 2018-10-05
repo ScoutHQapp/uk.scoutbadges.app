f31ac11c2499d932a7f0522604d47e0bff8669b8
David Kitchen <david.kitchen@commercelabs.io>
2018-10-05 10:40:35 +0100

    Add Google Site verification

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,8 +7,8 @@ import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+    <StaticQuery
+        query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -17,35 +17,36 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+        render={data => (
+            <>
+                <Helmet
+                    title={data.site.siteMetadata.title}
+                    meta={[
+                        { name: 'description', content: 'Sample' },
+                        { name: 'keywords', content: 'sample, something' },
+                        {name: 'google-site-verification', content: 'kLoDBiTpAOAkT_n3T1ENlneIpuVnGdMmJULrzTTCYn0'},
+                    ]}
+                >
+                    <html lang="en" />
+                </Helmet>
+                <Header siteTitle={data.site.siteMetadata.title} />
+                <div
+                    style={{
+                        margin: '0 auto',
+                        maxWidth: 960,
+                        padding: '0px 1.0875rem 1.45rem',
+                        paddingTop: 0,
+                    }}
+                >
+                    {children}
+                </div>
+            </>
+        )}
+    />
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
