376c13a961e7fe356ce1f711f801243f70cd1e85
David Kitchen <david.kitchen@commercelabs.io>
2018-10-05 10:20:10 +0100

    Add Sitemap.xml and Robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,58 +1,68 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Scout Badges',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-      `gatsby-plugin-catch-links`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'UK Scout Badges App',
-        short_name: 'Scout Badges',
-        start_url: '/',
-        background_color: '#7413dc',
-        theme_color: '#7413dc',
-        display: 'standalone',
-        icon: 'src/images/logo.png', // This path is relative to the root of the site.
-      },
+    siteMetadata: {
+        title: 'Scout Badges',
+        siteUrl: 'https://uk.scoutbadges.app',
     },
-    'gatsby-plugin-offline',
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            path: `${__dirname}/src/pages`,
-            name: "markdown-pages",
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        `gatsby-plugin-catch-links`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'UK Scout Badges App',
+                short_name: 'Scout Badges',
+                start_url: '/',
+                background_color: '#7413dc',
+                theme_color: '#7413dc',
+                display: 'standalone',
+                icon: 'src/images/logo.png', // This path is relative to the root of the site.
+            },
         },
-    },
-    `gatsby-transformer-remark`,
-      {
-          resolve: `gatsby-plugin-google-analytics`,
-          options: {
-              trackingId: "UA-126973909-1",
-              // Puts tracking script in the head instead of the body
-              head: false,
-              // Setting this parameter is optional
-              anonymize: true,
-              // Setting this parameter is also optional
-              respectDNT: true,
-              // Avoids sending pageview hits from custom paths
-              //exclude: ["/preview/**", "/do-not-track/me/too/"],
-              // Enables Google Optimize using your container Id
-              //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-              // Any additional create only fields (optional)
-              sampleRate: 5,
-              siteSpeedSampleRate: 10,
-              cookieDomain: "uk.scoutbadges.app",
-          },
-      },
-      {
-          resolve: `gatsby-plugin-google-fonts`,
-          options: {
-              fonts: [
-                  `Nunito Sans\:300,400,400i,700` // you can also specify font weights and styles
-              ]
-          }
-      }
-  ],
+        'gatsby-plugin-offline',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: "markdown-pages",
+            },
+        },
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-126973909-1",
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                //exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Enables Google Optimize using your container Id
+                //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+                // Any additional create only fields (optional)
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: "uk.scoutbadges.app",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [
+                    `Nunito Sans\:300,400,400i,700` // you can also specify font weights and styles
+                ]
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+                host: 'https://uk.scoutbadges.app',
+                sitemap: 'https://uk.scoutbadges.app/sitemap.xml',
+                policy: [{ userAgent: '*', allow: '/' }]
+            }
+        }
+    ],
 }
